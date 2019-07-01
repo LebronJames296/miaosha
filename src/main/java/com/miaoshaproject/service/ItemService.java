90abf2803ec2a3eb2896536dc712ae94f07086bc
James <296466865@qq.com>
2019-07-01 11:41:17 +0800

    完成商品模型的建立(create，listitem)

---
+++ src/main/java/com/miaoshaproject/service/ItemService.java
@@ -0,0 +1,22 @@
+package com.miaoshaproject.service;
+
+import com.miaoshaproject.error.BusinessException;
+import com.miaoshaproject.service.model.ItemModel;
+
+import java.util.List;
+
+/**
+ * @Author 86178
+ * @create 2019/7/1 8:30
+ */
+public interface ItemService {
+
+    //创建商品
+    ItemModel createItem(ItemModel itemModel) throws BusinessException;
+
+    //商品列表浏览
+    List<ItemModel> listItem();
+
+    //商品详情浏览
+    ItemModel getItemById(Integer id);
+}
